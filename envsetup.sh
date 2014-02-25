18ee4d560658578e588133cfbb27e47cda8c1b1d
erikcas <erikcas1972@gmail.com>
2014-02-25 23:32:19 +0100

    add roomservice and repopick

--- envsetup.sh
+++ envsetup.sh
@@ -558,6 +558,20 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
+        # if we can't find the product, try to grab it from our github
+        T=$(gettop)
+        pushd $T > /dev/null
+        build/tools/roomservice.py $product
+        popd > /dev/null
+        check_product $product
+    else
+        T=$(gettop)
+        pushd $T > /dev/null
+        build/tools/roomservice.py $product true
+        popd > /dev/null
+    fi
+    if [ $? -ne 0 ]
+    then
         echo
         echo "** Don't have a product spec for: '$product'"
         echo "** Do you have the right repo manifest?"
@@ -1621,6 +1635,12 @@ function set_java_home() {
                 ;;
         esac
     fi
+}
+
+function repopick() {
+    set_stuff_for_environment
+    T=$(gettop)
+    $T/build/tools/repopick.py $@
 }
 
 # Print colored exit condition
