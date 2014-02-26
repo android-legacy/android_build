459ee4fc266a780d1a9271bb4c8bda3d87881a7a
erikcas <erikcas1972@gmail.com>
2014-02-26 22:08:37 +0100

    adjust remote

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -39,7 +39,7 @@ default_rem = "android-legacy"
 default_rev = "kitkat"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
-default_team_rem = "github"
+default_team_rem = "android-legacy"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
