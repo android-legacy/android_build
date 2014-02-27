d56a8ca46dccb1b7cbae0648f3f4a2dda663c1fa
erikcas <erikcas1972@gmail.com>
2014-02-27 22:47:47 +0100

    roomservice for android-legacy
    Change-Id: Id20c3345998d7db92619d2917f21f7766f795439

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -34,16 +34,16 @@ except ImportError:
 
 # Config
 # set this to the default remote to use in repo
-default_rem = "omnirom"
+default_rem = "github"
 # set this to the default revision to use (branch/tag name)
 default_rev = "android-4.4"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/omnirom"
-default_team_rem = "omnirom"
+default_team_rem = "github"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "omnirom"
+android_team = "android-legacy"
 
 
 def check_repo_exists(git_data):
